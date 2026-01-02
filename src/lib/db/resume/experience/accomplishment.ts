9147561f12592f70885d6f65183d128007951322
Christian Areas <me@areas.me>
2026-01-02 01:58:26 -0800

    Optimized getAccomplishmentByCandidateIdRoleIdAndAccomplishmentId query and refactored its code.

--- src/lib/db/resume/experience/accomplishment.ts
+++ src/lib/db/resume/experience/accomplishment.ts
@@ -1,7 +1,7 @@
 // Dependencies.
 import { and, eq } from "drizzle-orm"
 import { db } from "@/lib/db"
-import { accomplishments, candidates, roles } from "@/lib/db/schema"
+import { accomplishments, roles } from "@/lib/db/schema"
 
 // Get accomplishment by candidate ID, role ID, and accomplishment ID.
 export async function getAccomplishmentByCandidateIdRoleIdAndAccomplishmentId(
@@ -9,32 +9,30 @@ export async function getAccomplishmentByCandidateIdRoleIdAndAccomplishmentId(
 	roleId: string,
 	accomplishmentId: string,
 ) {
-	// Select candidate, role, and accomplishment.
+	// Select role and accomplishments.
 	const [accomplishment] = await db
 		.select({
-			candidateId: candidates.candidateId,
+			candidateId: roles.candidateId,
 			roleId: roles.roleId,
 			accomplishmentId: accomplishments.accomplishmentId,
 			accomplishment: accomplishments.accomplishment,
 			sortOrder: accomplishments.sortOrder,
 		})
-		.from(candidates)
-		.leftJoin(
-			roles,
-			and(
-				eq(roles.candidateId, candidates.candidateId),
-				eq(roles.roleId, roleId),
-			),
-		)
-		.leftJoin(
+		.from(roles)
+		.innerJoin(
 			accomplishments,
 			and(
-				eq(accomplishments.candidateId, candidates.candidateId),
-				eq(accomplishments.roleId, roles.roleId),
+				eq(roles.candidateId, accomplishments.candidateId),
+				eq(roles.roleId, accomplishments.roleId),
+			),
+		)
+		.where(
+			and(
+				eq(roles.candidateId, candidateId),
+				eq(roles.roleId, roleId),
 				eq(accomplishments.accomplishmentId, accomplishmentId),
 			),
 		)
-		.where(eq(candidates.candidateId, candidateId))
 		.limit(1)
 
 	return accomplishment ?? null
