eb0fca393b423d5532afb06d5b75ea7043b9d33a
Christian Areas <me@areas.me>
2026-01-02 14:46:04 -0800

    Refactored resume fetching.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+export const revalidate = 0
+
 // Dependencies.
 import { notFound } from "next/navigation"
 import Resume from "@/components/Resume"
-import { getResume } from "@/lib/data/resume"
 import { getFirstCandidateId } from "@/lib/db/resume/candidate/candidate"
+import { getResumeByCandidateId } from "@/lib/db/resume/resume"
 
 // Page.
 export default async function Home() {
@@ -11,7 +13,7 @@ export default async function Home() {
 	if (!candidateId) notFound()
 
 	// Resume.
-	const resume = await getResume(candidateId)
+	const resume = await getResumeByCandidateId(candidateId)
 	if (!resume) notFound()
 
 	// Render.
--- src/lib/data/resume.ts
+++
@@ -1,8 +0,0 @@
-// Dependencies.
-import { cache } from "react"
-import { getResumeByCandidateId } from "@/lib/db/resume/resume"
-
-// Cache resume.
-export const getResume = cache(async (candidateId: string) =>
-	getResumeByCandidateId(candidateId),
-)
